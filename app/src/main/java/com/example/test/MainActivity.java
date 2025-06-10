c39cad181efafd28e62a6a56efd7c44b4cf16d08
Abiba <134049091+wangpeng258@users.noreply.github.com>
2025-06-10 16:40:39 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // 这里填你需要打包的 H5 页面链接
-        webView.loadUrl("https://baidu.com");
+        webView.loadUrl("http://125.122.155.30");
 
         //显示一些小图片（头像）
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
